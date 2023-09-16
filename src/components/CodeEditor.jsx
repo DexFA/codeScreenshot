ab1259d23c5dde5be19de86aba1344136bce5aa3
Fidelis Adaikwu <110574901+DexFA@users.noreply.github.com>
2023-09-16 02:49:00 +0100

    Build code editor component.

--- src/components/CodeEditor.jsx
+++ src/components/CodeEditor.jsx
@@ -1,4 +1,7 @@
 import { cn } from "@/lib/utils";
+import { codeSnippets } from "@/options";
+import hljs from "highlight.js";
+import Editor from "react-simple-code-editor";
 
 const CodeEditor = () => {
 	return (
@@ -8,7 +11,33 @@ const CodeEditor = () => {
 				"bg-black/75 border-gray-600/40"
 			)}
 		>
-			CodeEditor
+			<header className="grid grid-cols-6 gap-3 items-center px-4 py-3">
+				<div className="flex gap-1.5">
+					<div className="rounded-full h-3 w-3 bg-red-500"></div>
+					<div className="rounded-full h-3 w-3 bg-yellow-500"></div>
+					<div className="rounded-full h-3 w-3 bg-green-500"></div>
+				</div>
+				<div className="col-span-4 flex justify-center">
+					<input
+						type="text"
+						value="Untitled"
+						spellCheck={false}
+						onClick={(e) => e.target.select()}
+						className="bg-transparent text-center text-gray-400 text-sm font-medium focus:outline-none"
+					/>
+				</div>
+			</header>
+			<div className={cn("px-4 pb-4")}>
+				<Editor
+					value={codeSnippets[0].code}
+					highlight={(code) =>
+						hljs.highlight(code, { language: codeSnippets[0].language }).value
+					}
+					style={{ fontStyle: "JetBrains Mono", fontSize: 18 }}
+					textareaClassName="focus:outline-none"
+					onClick={(e) => e.target.select()}
+				/>
+			</div>
 		</div>
 	);
 };
