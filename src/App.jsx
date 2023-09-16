0ed7a32840fac699b89b662b050007c6a5e936c0
Fidelis Adaikwu <110574901+DexFA@users.noreply.github.com>
2023-09-16 02:28:24 +0100

    Setup Project and Create main component-CodeEditor

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import CodeEditor from "./components/CodeEditor";
+
+function App() {
+	return (
+		<main className="dark min-h-screen flex justify-center items-center bg-neutral-950 text-white">
+			<CodeEditor />
+		</main>
+	);
+}
+
+export default App;
---
+++ src/components/CodeEditor.jsx
@@ -0,0 +1,16 @@
+import { cn } from "@/lib/utils";
+
+const CodeEditor = () => {
+	return (
+		<div
+			className={cn(
+				"min-w-[400px] border-2 rounded-xl shadow-2xl",
+				"bg-black/75 border-gray-600/40"
+			)}
+		>
+			CodeEditor
+		</div>
+	);
+};
+
+export default CodeEditor;
